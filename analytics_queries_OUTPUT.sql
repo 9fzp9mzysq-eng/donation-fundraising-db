48b4edce47b32fa35161745270a99b7a93c38fce
9fzp9mzysq-eng <9fzp9mzysq@privaterelay.appleid.com>
2025-12-11 18:32:11 -0500

    Add Oracle output results for advanced analytical queries

---
+++ analytics_queries_OUTPUT.sql
@@ -0,0 +1,37 @@
+
+CAMPAIGN_ID TITLE                                                                                                                                                  TARGET_AMOUNT TOTAL_RAISED PERCENT_COMPLETED
+----------- ------------------------------------------------------------------------------------------------------------------------------------------------------ ------------- ------------ -----------------
+          1 Support Orphan Education                                                                                                                                        5000          100                 2
+          2 Rebuild Ahmed's Home                                                                                                                                           15000          150                 1
+
+
+   USER_ID DONOR_NAME                                                                                            TOTAL_DONATED
+---------- ----------------------------------------------------------------------------------------------------- -------------
+         3 Sara Johnson                                                                                                    150
+
+
+DONATION_ DAILY_TOTAL RUNNING_TOTAL
+--------- ----------- -------------
+01-DEC-24         100           100
+02-DEC-24         150           250
+
+
+CAMPAIGN_ID TOTAL_RAISED DONATION_RANK
+----------- ------------ -------------
+          2          150             1
+          1          100             2
+
+no rows selected
+
+BENEFICIARY_ID NAME                                                                                                 TOTAL_DONATED_TO_CAMPAIGNS TOTAL_DISBURSED NET_BALANCE
+-------------- ---------------------------------------------------------------------------------------------------- -------------------------- --------------- -----------
+             2 Ahmed El Fassi                                                                                                              150             800        -650
+             1 Hope Children Foundation                                                                                                    100             500        -400
+
+
+  DONOR_ID CAMPAIGNS_SUPPORTED
+---------- -------------------
+         3                   1
+         2                   1
+
+no rows selected
